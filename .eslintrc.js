a26b4206cd4ea64d9f45fb911951a54a374f6736
Van Chi Hieu <vanchihieu3@gmail.com>
2024-07-06 21:21:03 +0700

    Dropdown picker

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    '@typescript-unused-imports/no-unused-imports': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-inline-styles': 0,
+    'react-native/no-inline-styles': 0,
+  },
 };
